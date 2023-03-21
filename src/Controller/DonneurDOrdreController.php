e40464b6f4d5978f84c1a1b00a4d618a02b49ddc
Olwen Wempe <wempe.olwen@gmail.com>
2023-03-21 16:25:12 +0100

    initial setup and minor error fixes

--- src/Controller/DonneurDOrdreController.php
+++ src/Controller/DonneurDOrdreController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/donneur/d/ordre')]
+#[Route('/client')]
 class DonneurDOrdreController extends AbstractController
 {
     #[Route('/', name: 'app_donneur_d_ordre_index', methods: ['GET'])]
